418ef56df311b7a70d81be5380a9b1519a19a162
unknown <xfvjykk@yandex.ru>
2022-11-19 12:19:31 +0300

    Второе задание

--- Program.cs
+++ Program.cs
@@ -13,3 +13,26 @@ else
 {
     Console.WriteLine($"Max = {number2} Min = {number1}");
 }
+
+
+Console.WriteLine("Введите первое число");
+int number12 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number22 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number32 = Convert.ToInt32(Console.ReadLine());
+if (number12 > number22)
+if (number12 > number32)
+{
+    Console.WriteLine($"Максимальное {number12}");
+}
+if (number22 > number12)
+if (number22 > number32)
+{
+    Console.WriteLine($"Максимальное {number22}");
+}
+else
+{
+    Console.WriteLine($"Максимальное {number32}");
+}
+
